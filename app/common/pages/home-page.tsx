65a560cdacc128e520e9a3fc95aaf5a21b68455c
Debora-k <debora0104k@gmail.com>
2025-02-03 15:32:13 -0700

    added product cards on home-page

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,9 +1,37 @@
 import type { MetaFunction } from "react-router";
+import { ProductCard } from "~/features/products/components/product-card";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Home | wemake" },
+    { name: "description", content: "Welcome to wemake!" },
+  ];
+};
 
 export default function HomePage() {
   return (
-    <main className="container mx-auto px-4 py-8">
-      <h1></h1>
-    </main>
+    <div className="px-20">
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Today's Products
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The best products made by our wemake community today.
+          </p>
+        </div>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <ProductCard
+            key={index}
+            id={`productId-${index}`}
+            name="Product Name"
+            description="Product Description"
+            commentsCount={12}
+            viewsCount={12}
+            votesCount={120}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
