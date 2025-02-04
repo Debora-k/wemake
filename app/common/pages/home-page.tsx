3fbf2a2c76ebfecba8aa00141d6992faa677d0cf
Debora-k <debora0104k@gmail.com>
2025-02-03 17:03:55 -0700

    added post cards

--- app/common/pages/home-page.tsx
+++ app/common/pages/home-page.tsx
@@ -1,5 +1,8 @@
+import { Link } from "react-router";
 import type { MetaFunction } from "react-router";
 import { ProductCard } from "~/features/products/components/product-card";
+import { Button } from "../components/ui/button";
+import { PostCard } from "~/features/community/components/post-card";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +13,7 @@ export const meta: MetaFunction = () => {
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">
@@ -19,8 +22,11 @@ export default function HomePage() {
           <p className="text-xl font-light text-foreground">
             The best products made by our wemake community today.
           </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/products/leaderboards">Explore Leaderboard &rarr;</Link>
+          </Button>
         </div>
-        {Array.from({ length: 10 }).map((_, index) => (
+        {Array.from({ length: 11 }).map((_, index) => (
           <ProductCard
             key={index}
             id={`productId-${index}`}
@@ -29,6 +35,29 @@ export default function HomePage() {
             commentsCount={12}
             viewsCount={12}
             votesCount={120}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">
+            Latest Discussions
+          </h2>
+          <p className="text-xl font-light text-foreground">
+            The latest discussions from our wemake community.
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <PostCard
+            id="postId"
+            title="What is the best productivity tool?"
+            author="Debora"
+            authorAvatar="https://github.com/apple.png"
+            category="Productivity"
+            createdAt="12 hours ago"
           />
         ))}
       </div>
