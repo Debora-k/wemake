2a649ccf6df80e3533881108e149de0b1268a48f
Debora-k <debora0104k@gmail.com>
2025-02-13 18:46:04 -0700

    added job pages

---
+++ app/features/jobs/pages/submit-job-page.tsx
@@ -0,0 +1,11 @@
+import type { Route } from "./+types/submit-job-page";
+import type { ComponentProps } from "react";
+
+export default function SubmitJobPage() {
+  return (
+    <div className="container py-8">
+      <h1 className="text-3xl font-bold mb-6">Submit a Job</h1>
+      {/* Job submission form will go here */}
+    </div>
+  );
+}
