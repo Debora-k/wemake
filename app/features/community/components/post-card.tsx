fddc0500d0ac65b7c0eded6c3ddea14bf0780fcd
Debora-k <debora0104k@gmail.com>
2025-02-16 23:30:58 -0700

    added a community page

--- app/features/community/components/post-card.tsx
+++ app/features/community/components/post-card.tsx
@@ -11,7 +11,9 @@ import {
   AvatarImage,
 } from "~/common/components/ui/avatar";
 import { Button } from "~/common/components/ui/button";
-import { DotIcon } from "lucide-react";
+import { ChevronUpIcon, DotIcon } from "lucide-react";
+import { cn } from "~/lib/utils";
+
 interface PostCardProps {
   id: string;
   title: string;
@@ -19,6 +21,8 @@ interface PostCardProps {
   authorAvatar: string;
   category: string;
   createdAt: string;
+  expended?: boolean;
+  votesCount?: number;
 }
 
 export function PostCard({
@@ -28,10 +32,17 @@ export function PostCard({
   authorAvatar,
   category,
   createdAt,
+  expended = false,
+  votesCount = 0,
 }: PostCardProps) {
   return (
-    <Link to={`/community/${id}`}>
-      <Card className="bg-transparent hover:bg-card/50 transition-colors">
+    <Link to={`/community/${id}`} className="block">
+      <Card
+        className={cn(
+          "bg-transparent hover:bg-card/50 transition-colors",
+          expended ? "flex flex-row items-center justify-between" : ""
+        )}
+      >
         <CardHeader className="flex flex-row items-center gap-2">
           <Avatar className="size-12">
             <AvatarFallback>{author[0]}</AvatarFallback>
@@ -47,11 +58,19 @@ export function PostCard({
             </div>
           </div>
         </CardHeader>
-        <CardFooter className="flex justify-end">
-          <Button variant="link" asChild>
-            Reply &rarr;
-          </Button>
-        </CardFooter>
+        {!expended && (
+          <CardFooter className="flex justify-end">
+            <Button variant="link">Reply &rarr;</Button>
+          </CardFooter>
+        )}
+        {expended && (
+          <CardFooter className="flex justify-end pb-0">
+            <Button variant="outline" className="flex flex-col h-14">
+              <ChevronUpIcon className="size-4 shrink-0" />
+              <span>{votesCount}</span>
+            </Button>
+          </CardFooter>
+        )}
       </Card>
     </Link>
   );
---
+++ app/features/community/pages/submit-page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "~/common/components/hero";
+
+export default function SubmitPage() {
+  return (
+    <div>
+      <Hero title="" />
+    </div>
+  );
+}
