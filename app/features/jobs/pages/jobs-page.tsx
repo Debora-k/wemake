0987100020d87b78fb55f5d5aea2990468b0c8bf
Debora-k <debora0104k@gmail.com>
2025-02-24 21:30:55 -0700

    added responsive design for this web

--- app/features/jobs/pages/jobs-page.tsx
+++ app/features/jobs/pages/jobs-page.tsx
@@ -23,8 +23,8 @@ export default function JobsPage() {
   return (
     <div className="space-y-20">
       <Hero title="Jobs" subtitle="Companies looking for top talent" />
-      <div className="grid grid-cols-6 gap-20 items-start">
-        <div className="grid grid-cols-3 col-span-4 gap-5">
+      <div className="grid grid-cols-1 xl:grid-cols-6gap-20 items-start">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:col-span-4 gap-5">
           {Array.from({ length: 20 }).map((_, index) => (
             <JobCard
               key={index}
@@ -40,7 +40,7 @@ export default function JobsPage() {
             />
           ))}
         </div>
-        <div className="col-span-2 sticky top-20 flex flex-col gap-10">
+        <div className="xl:col-span-2 sticky top-20 flex flex-col gap-10">
           <div className="flex flex-col items-start gap-2.5">
             <h4 className="text-sm font-bold text-muted-foreground">Type</h4>
             <div className="flex flex-wrap gap-2">
