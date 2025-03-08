085d85bfeafc3b826a0ed951030ae09454e095ec
Debora-k <debora0104k@gmail.com>
2025-03-07 22:31:25 -0700

    added a db connection for categories page

--- app/features/products/pages/categories-page.tsx
+++ app/features/products/pages/categories-page.tsx
@@ -1,23 +1,28 @@
 import type { Route } from "./+types/categories-page";
 import { Hero } from "~/common/components/hero";
 import { CategoryCard } from "~/features/products/components/category-card";
-
+import { getCategories } from "../queries";
 export const meta: Route.MetaFunction = () => [
   { title: "Categories | Product Hunt Clone" },
   { name: "description", content: "Browse products by category" },
 ];
 
-export default function CategoriesPage() {
+export const loader = async () => {
+  const categories = await getCategories();
+  return { categories };
+};
+
+export default function CategoriesPage({ loaderData }: Route.ComponentProps) {
   return (
     <div className="space-y-10">
       <Hero title="Categories" subtitle="Browse products by category" />
       <div className="grid grid-cols-4 gap-10">
-        {Array.from({ length: 10 }).map((_, index) => (
+        {loaderData.categories.map((category) => (
           <CategoryCard
-            key={index}
-            id={`categoryId-${index}`}
-            name="Category Name"
-            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos."
+            key={category.category_id}
+            id={category.category_id}
+            name={category.name}
+            description={category.description}
           />
         ))}
       </div>
--- app/features/products/pages/category-page.tsx
+++ app/features/products/pages/category-page.tsx
@@ -2,6 +2,12 @@ import type { Route } from "./+types/category-page";
 import { Hero } from "~/common/components/hero";
 import { ProductCard } from "../components/product-card";
 import { ProductPagination } from "~/common/components/product-pagination";
+import {
+  getCategory,
+  getCategoryPages,
+  getProductsByCategory,
+} from "../queries";
+import { z } from "zod";
 
 export const meta = ({ params }: Route.MetaArgs) => {
   return [
@@ -10,28 +16,50 @@ export const meta = ({ params }: Route.MetaArgs) => {
   ];
 };
 
-export default function CategoryPage() {
+const paramsSchema = z.object({
+  category: z.coerce.number(),
+});
+
+export const loader = async ({ params, request }: Route.LoaderArgs) => {
+  const url = new URL(request.url);
+  const page = url.searchParams.get("page") || "1";
+  const { data, success } = paramsSchema.safeParse(params);
+  if (!success) {
+    throw new Error("Invalid category");
+  }
+  const [category, products, totalPages] = await Promise.all([
+    getCategory(data.category),
+    getProductsByCategory({
+      categoryId: data.category,
+      page: Number(page),
+    }),
+    getCategoryPages(data.category),
+  ]);
+  return { category, products, totalPages };
+};
+
+export default function CategoryPage({ loaderData }: Route.ComponentProps) {
   return (
     <div className="space-y-10">
       <Hero
-        title="Developer Tools"
-        subtitle={`Tools for developers to build products faster!`}
+        title={loaderData.category.name}
+        subtitle={loaderData.category.description}
         className="text-lg"
       />
       <div className="space-y-5 w-full max-w-screen-md mx-auto">
-        {Array.from({ length: 11 }).map((_, index) => (
+        {loaderData.products.map((product) => (
           <ProductCard
-            key={index}
-            id={`productId-${index}`}
-            name="Product Name"
-            description="Product Description"
-            commentsCount={12}
-            viewsCount={12}
-            votesCount={120}
+            key={product.product_id}
+            id={product.product_id}
+            name={product.name}
+            description={product.description}
+            reviewsCount={product.reviews}
+            viewsCount={product.views}
+            votesCount={product.upvotes}
           />
         ))}
       </div>
-      <ProductPagination totalPages={10} />
+      <ProductPagination totalPages={loaderData.totalPages} />
     </div>
   );
 }
