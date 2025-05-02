72b5cdbb5b3856cef0e326a9ea5897fdf95758f7
Debora-k <debora0104k@gmail.com>
2025-05-01 22:15:15 -0600

    fixed a bug when users click others, leads to right page

--- app/features/community/components/reply.tsx
+++ app/features/community/components/reply.tsx
@@ -70,7 +70,7 @@ export function Reply({
         </Avatar>
         <div className="flex flex-col gap-4 items-start w-full">
           <div className="flex items-center gap-2">
-            <Link to={`users/@${username}`}>
+            <Link to={`/users/${username}`}>
               <h4 className="font-medium">{name}</h4>
             </Link>
             <span className="text-xs text-muted-foreground">
